9431813b2ee830c3336e5504ed6623a7a917187a
Liang Li <liliang6@me.com>
2026-04-23 16:38:43 +0800

    build: fix file2alias uuid_t host includes

--- scripts/mod/file2alias.c
+++ scripts/mod/file2alias.c
@@ -47,6 +47,10 @@ typedef struct {
 	__u8 b[16];
 } uuid_le;
 
+typedef struct {
+	__u8 b[16];
+} uuid_t;
+
 /* Big exception to the "don't include kernel headers into userspace, which
  * even potentially has different endianness and word sizes, since
  * we handle those differences explicitly below */
